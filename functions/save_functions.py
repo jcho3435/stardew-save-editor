0bbdd2a3aa9496c573f80ec96bc991e6b09779f6
Jason <jaseagle2003@gmail.com>
2024-07-19 18:50:07 -0400

    Saving level data is now working!

--- functions/save_functions.py
+++ functions/save_functions.py
@@ -1,4 +1,4 @@
-from components.constants import Keys, CharacterSavePaths, WorldSavePaths
+from components.constants import Keys, WorldSavePaths
 from lxml import etree
 from functions.get_and_load_xml import get_xml_roots
 from functions.functions import get_current_time
@@ -6,10 +6,13 @@ from functions.functions import get_current_time
 def save_name_to_tree(farmer: etree._Element, name: str):
     farmer.xpath("./name[1]")[0].text = name.strip()
 
-# def save_skills_to_tree(farmer, skill):
-#     pass
+def save_skill_levels_to_tree(farmer: etree._Element, values: dict, index):
+    for skill, keys in Keys._FarmerSkillLevels.items():
+        farmer.xpath(f"./{skill}Level[1]")[0].text = values[keys[index]].strip()
 
-def save_farmer_data_to_tree(values: dict) -> str:
+
+
+def save_farmers_tab_data_to_tree(values: dict) -> str:
     event_string = ""
     character_data, world_data = get_xml_roots()
 
@@ -18,23 +21,27 @@ def save_farmer_data_to_tree(values: dict) -> str:
 
     #Change in character saves
     save_name_to_tree(character_data, farmerName)
+    save_skill_levels_to_tree(character_data, values, 0)
 
-    event_string += f"[{get_current_time()}] Host farmer changes saved to character xml tree.\n"
+    event_string += f"[{get_current_time()}] Farmer tab host farmer changes saved to character xml tree.\n"
 
     #Change in world saves
     worldsave_farmer = world_data.xpath(WorldSavePaths._Farmer)[0] #This is the <player> tag for the host player
     save_name_to_tree(worldsave_farmer, farmerName)
+    save_skill_levels_to_tree(worldsave_farmer, values, 0)
 
-    event_string += f"[{get_current_time()}] Host farmer changes saved to world xml tree.\n"
+    event_string += f"[{get_current_time()}] Farmer tab host farmer changes saved to world xml tree.\n"
     
 
     # Save farmhand data
     farmhands = world_data.xpath(WorldSavePaths._Farmhands) # gets list of farmhand <Farmer> tags
     index = 1
-    for player in farmhands:
-        save_name_to_tree(player, values[Keys._FarmerNames[index]])
+    for farmer in farmhands:
+        save_name_to_tree(farmer, values[Keys._FarmerNames[index]])
+        save_skill_levels_to_tree(farmer, values, index)
         index += 1
 
-    event_string += f"[{get_current_time()}] Farmhand changes saved to world xml tree.\n\n"
+    if index != 1:
+        event_string += f"[{get_current_time()}] Farmer tab farmhand changes saved to world xml tree.\n\n"
 
     return event_string
