260139a13df01f04c2c2c4fb2ac47ba6f00a6016
Jason <jaseagle2003@gmail.com>
2024-07-20 21:08:36 -0400

    basic docs pages created. They kinda suck lol

--- scripts/build_app.sh
+++ scripts/build_app.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-pyinstaller -wF -i "icons/app-icon.ico" -n "StardewSaveEditor" --add-data "icons:icons" --add-data "docs:docs" runner.py
+# I'm running this with git bash
+# App made for windows, has a script folder with both a windows script and a bash script. Smart. I know.
+
+name="StardewSaveEditor"
+pyinstaller -wF -i "icons/app-icon.ico" -n "$name" --add-data "icons:icons" --add-data "docs:docs" runner.py
+
+
+cp scripts/clean_logs.bat dist/
+
+mv dist/ $name/
