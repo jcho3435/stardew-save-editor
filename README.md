b6dec3f68c40d17c06ee8aef95681c663b7d1cca
Jason <jaseagle2003@gmail.com>
2024-07-26 00:52:20 -0400

    modified readme

--- README.md
+++ README.md
@@ -8,13 +8,3 @@ All error logs and event logs are located in `logs/`<br>
 Logs can be cleaned by running `clean_logs.bat`, located in scripts/
 
 Construct app using `scripts/build_app.sh`.<br>
-<br>
-<br>
-File structure for release:<br>
-StardewSaveEditor/<br>
-├── Scripts/<br>
-│   ├── Scripts.py<br>
-│   └── Scripts.bat<br>
-├── README.md<br>
-├── StardewSaveEditor.exe<br>
-└── LICENSE<br>
---
+++ testrun.py
@@ -0,0 +1,7 @@
+# TODO: Test the issue occurring on the backups tab
+
+# Create a tab layout
+# All of the contents of the tab should be encapsulated in a column- scrollable, expand_x, and expand_y, vscroll only
+# Inside the column, have a button to add frames with a listbox size (40, 12) and a button in the same row
+# Have another column with a push() element in the same row. Frame elements get added to this column
+# After every addition of a frame, call contents_changed()
